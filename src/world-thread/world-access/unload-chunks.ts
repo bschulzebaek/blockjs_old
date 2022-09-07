88ce5d6286cbd74f848d729bcb910b392e563997
Benedikt Schulze Baek <b.schulze-baek@shopware.com>
2022-09-07 12:00:22 +0200

    Only save chunks after changes, not during unloading

--- src/world-thread/world-access/unload-chunks.ts
+++ src/world-thread/world-access/unload-chunks.ts
@@ -3,9 +3,7 @@ import WorldContainer from '../WorldContainer';
 export default async function unloadChunks(chunks: string[]) {
     const world = WorldContainer.getWorld();
 
-    const saveChunks = chunks.map((key) => {
+    chunks.forEach((key) => {
         return world.popChunk(key);
     });
-
-    await WorldContainer.getChunkRepository().writeList(saveChunks);
 }
