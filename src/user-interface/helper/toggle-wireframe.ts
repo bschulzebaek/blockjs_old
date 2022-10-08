e80386d16377b48b32f55a15ffb429b6e9d3946b
Benedikt Schulze Baek <b.schulze-baek@shopware.com>
2022-10-09 00:57:00 +0200

    Use correct event property for key handler

--- src/user-interface/helper/toggle-wireframe.ts
+++ src/user-interface/helper/toggle-wireframe.ts
@@ -2,7 +2,7 @@ import ThreadManager, { ThreadNames } from '../../threads/main/thread-manager/Th
 import { RenderMessages } from '../../shared/messages/ThreadMessages';
 
 export default function toggleWireframe(event: KeyboardEvent) {
-    if (event.code === 'Dead') {
+    if (event.key === 'Dead') {
         ThreadManager.send(ThreadNames.RENDER, RenderMessages.SET_DRAW_MODE);
     }
 }
