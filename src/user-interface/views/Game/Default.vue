1927a4aff1e30d3d70f688b64429344f84370978
Benedikt Schulze Baek <b.schulze-baek@shopware.com>
2022-10-09 15:48:23 +0200

    Add fps counter to Game/Default.vue

--- src/user-interface/views/Game/Default.vue
+++ src/user-interface/views/Game/Default.vue
@@ -3,6 +3,10 @@
         <span class="crosshair center-absolute">x</span>
 
         <toolbar />
+
+        <div class="game-default__debug">
+            <div class="fps">{{ fps }}</div>
+        </div>
     </div>
 </template>
 
@@ -12,5 +16,25 @@ export default {
     components: {
         Toolbar
     },
+    data() {
+        return {
+            fps: 0,
+        }
+    },
+    mounted() {
+        window.requestAnimationFrame(this.loop.bind(this, 0));
+    },
+    methods: {
+        loop(time: number, lastTime: number) {
+            const delta = this.getDelta(time, lastTime);
+
+            this.fps = (1 / delta).toFixed(1);
+
+            requestAnimationFrame((newTime) => this.loop(newTime, time));
+        },
+        getDelta(time: number, lastTime: number) {
+            return Math.min(time - lastTime, 100) / 1000;
+        }
+    }
 };
 </script>
